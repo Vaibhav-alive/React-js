af599739e29421a0c71f255404b5fc8434a987c4
Vaibhav <vaibhavkumar20241@gmail.com>
2025-08-30 12:51:49 +0530

    animation

--- src/Component/list.jsx
+++ src/Component/list.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
+import { useRef, useEffect } from 'react'
 import Genrelist from '../constant/genrelist'
 import MovieCard from './MovieCard'
 function list() {
- 
+  const card = useRef([])
+  useEffect(() => {
+
+       const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+          }
+          else{
+            entry.target.classList.remove('show') 
+          }
+        })
+      },
+      { threshold: 0.5 } // adjust threshold as needed
+    )
+    card.current.forEach((section) => {
+      if (section) observer.observe(section)
+    })
+    return () => {
+      card.current.forEach((section) => {
+        if (section) observer.unobserve(section)
+      })
+    }
+
+  }, [])
+
   return (
-    
+
     <div className="list" >
-        {Genrelist.genere.map((item, index)=>index<=4&&(
-            <section key={item.id}>
-                <h2>{item.name}</h2>
-                <MovieCard gid={item.id}  />
-            </section>
-        ))}
-        
+      {Genrelist.genere.map((item, index) => index <= 4 && (
+        <section key={item.id} ref={(el) => (card.current[index] = el)}>
+          <h2>{item.name}</h2>
+          <MovieCard gid={item.id} />
+        </section>
+      ))}
+
     </div>
   )
 }
